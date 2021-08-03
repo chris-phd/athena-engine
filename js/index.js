88905011a452ad3d9304efcf46e756029cbcfb39
Chris Cooper <cjcooper1998@gmail.com>
2021-08-03 23:02:47 +1000

    Begin adding user config GUI elements.

--- js/index.js
+++ js/index.js
@@ -15,18 +15,45 @@ function setupPage() {
     // Setup the chess pieces
     setupChessBoardSquares();
 
-    // Assign the squares the correct event handlers
-    var squares = document.getElementsByClassName("square");
-    for (var i = 0; i < squares.length; ++i) {
-        squares[i].ondragover = onDragOver;
-        squares[i].ondrop = onDrop;
-    }
-
     // Set the chess pieces
     startingPositionFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     setBoardFromFenString(startingPositionFen);
 
+    // Setup the user config GUI
+    setupConfigGUI();
+
     console.log("bottom of setup page");
+}
+
+// Config GUI
+function setupConfigGUI() {
+
+    var resetButon = document.getElementById("reset-board-button");
+    resetButon.onclick = resetBoard;
+}
+
+function resetBoard() {
+    console.log("resetBoard");
+
+    var whitePlayerElement = document.getElementById("white-player-type");
+    var blackPlayerElement = document.getElementById("black-player-type");
+    var chessPositionElement = document.getElementById("chess-position");
+
+    var whitePlayer = whitePlayerElement.options[whitePlayerElement.selectedIndex].value;
+    var blackPlayer = blackPlayerElement.options[blackPlayerElement.selectedIndex].value;
+    var chessPosition = chessPositionElement.options[chessPositionElement.selectedIndex].value;
+
+    console.log("selected options:");
+    console.log(whitePlayer);
+    console.log(blackPlayer);
+    console.log(chessPosition);
+
+    var fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    if (chessPosition == "Test Queen") {
+        fenString = "8/6R1/3n4/8/1r1Q4/8/4p1P1/K1k5 w KQkq - 0 1";
+    }
+    setBoardFromFenString(fenString);
+
 }
 
 // Piece event handlers
@@ -47,6 +74,14 @@ function onDrop(event) {
     var square;
     if (isCaptureMove(event.target)) {
         var capturedPiece = event.target;
+        var capturingPiece = document.getElementById(id);
+        
+        // Piece was dropped back onto the same square
+        if (capturedPiece == capturingPiece) {
+            return;
+        }
+
+        // Remove the captured piece from the board
         square = event.target.parentElement;
         square.removeChild(capturedPiece);
     } else {
@@ -97,11 +132,21 @@ function setupChessBoardSquares() {
 
         ++colourInx;
     }
+
+    // Assign the squares the correct event handlers
+    var squares = document.getElementsByClassName("square");
+    for (var i = 0; i < squares.length; ++i) {
+        squares[i].ondragover = onDragOver;
+        squares[i].ondrop = onDrop;
+    }
+
 }
 
 function setBoardFromFenString(fenString) {
     // fenBoardString describes a board position using Forsyth-Edwarsd notation.
     // https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+
+    clearBoard();
 
     // Read the fen string and set the corresponding board position.
     // Begin setting the board in the top left square (a8)
@@ -121,6 +166,30 @@ function setBoardFromFenString(fenString) {
         } else {
             console.log("%s is an unrecognised character", fenString[i]);
         }
+    }
+}
+
+function clearBoard() {
+    for (var rank = 1; rank <= 8; ++rank) {
+        for (var file = 1; file <= 8; ++file) {
+            clearSquare(rank, file);
+        }
+    }
+}
+
+function clearSquare(rank, file) {
+    var squareCode = squareFromRankAndFile(rank, file);
+    square = document.getElementById(squareCode);
+    if (square != null) {
+
+        // Find all pieces attached to the square and remove them
+        var pieces = square.childNodes;
+        pieces.forEach(piece => {
+            square.removeChild(piece);
+        });
+
+    } else {
+        console.log("Could not set %s at square %s. Square not found.", htmlId, squareCode);
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,35 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console::log_1(&JsValue::from_str("[lib::main_js]: Athena Engine"));
 
     Ok(())
+}
+
+//
+// The functions that the JavaScript front end interfaces with
+//
+
+#[wasm_bindgen]
+pub fn new_game(board_setup : usize, players : usize, is_white_turn : bool) {
+    console::log_1(&JsValue::from_str("[lib::new_game]: "));
+
+
+}
+
+#[wasm_bindgen]
+pub fn update_board() -> bool {
+    // Returns is_update_sucessful. If update fails, the front end is notified
+    // that the last move failed.
+
+    console::log_1(&JsValue::from_str("[lib::update_board]: "));
+
+    return true;
+}
+
+#[wasm_bindgen]
+pub fn ai_player_move() {
+
+    console::log_1(&JsValue::from_str("[lib::ai_player_move]: "));
+
 }
